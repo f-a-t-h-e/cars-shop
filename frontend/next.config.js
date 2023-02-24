0dc178fdb565e5d03686fe956c659d62fa69ee0e
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-02-24 09:29:42 +0200

    twin setup. tailwind & styled-components.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTwin = require("./withTwin.js");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withTwin({
+  reactStrictMode: true,
+  // compiler: {
+  //   styledComponents: true,
+  // },
+  // webpack: (config, { isServer }) => {
+  //   // Unset client-side javascript that only works server-side
+  //   config.resolve.fallback = { fs: false, module: false, path: false };
+  //   // if (!isServer) {
+  //   //   config.node = { fs: "empty", module: "empty" };
+  //   // }
+  //   return config;
+  // },
+});
+
+module.exports = nextConfig;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Head from "next/head";
+import React from "react";
+import tw from "twin.macro";
+import { Button, Logo } from "@/components";
 
-export default function Home() {
-  return <div>السلام عليكم و رحمة الله و بركاتة</div>;
-}
+const styles = {
+  // Move long class sets out of jsx to keep it scannable
+  container: ({ hasBackground }: { hasBackground: boolean }) => [
+    tw`flex flex-col items-center justify-center h-screen`,
+    hasBackground && tw`bg-gradient-to-b from-electric to-ribbon`,
+  ],
+};
+
+const IndexPage = () => (
+  <div css={styles.container({ hasBackground: true })}>
+    <div tw="flex flex-col justify-center h-full gap-y-5">
+      <Button variant="primary">Submit</Button>
+      <Button variant="secondary">Cancel</Button>
+      <Button isSmall>Close</Button>
+    </div>
+    <Logo />
+  </div>
+);
+
+export default IndexPage;
